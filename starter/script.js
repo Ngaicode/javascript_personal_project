78d1a210dac6aae8e897c0c1137f138c85da5423
Ngaicode <Mjombakevin376@gmail.com>
2021-12-19 10:55:38 +0300

    activating strict mode

---
+++ starter/script.js
@@ -0,0 +1,11 @@
+'use strict'; //placing this string as the first statement of every script file activates strict mode
+
+let hasDriversLiscence = false;
+const passDriversTest = true;
+
+if (passDriversTest) hasDriversLiscence = true;
+if (hasDriversLiscence) {
+    console.log('i can drive :D')
+}
+
+// const interface = 'audio';
