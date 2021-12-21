461059ab3101cb5872348c72ef5f9ba0bec42537
Ngaicode <Mjombakevin376@gmail.com>
2021-12-21 11:34:17 +0300

    understanding functions

--- starter/script.js
+++ starter/script.js
@@ -21,9 +21,15 @@ logger();
 logger();
 
 
+function fruitProcessor(apples, oranges) {
+    const juice = `juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+const applejuice = fruitProcessor(5, 0);
+console.log(applejuice);
 
-
-
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
 
 
 
