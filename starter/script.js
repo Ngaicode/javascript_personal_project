185df6d4f6e43f08c30879d2f0c2afba3ff46dc4
Ngaicode <Mjombakevin376@gmail.com>
2022-01-18 16:45:15 +0300

    comments about continue

--- starter/script.js
+++ starter/script.js
@@ -500,6 +500,7 @@ console.log(ages);
 console.log('___Only strings___')
 for (let i = 0; i <= kevinArray.length - 1; i++) {
     if (typeof (kevinArray[i]) !== 'string') continue;
+    // if continue is activated,the current iteration is immediately exited,so the lines of code coming after it in the for loop will not be exited in the current iteration
     console.log(kevinArray[i], typeof (kevinArray[i]));
 
 };
