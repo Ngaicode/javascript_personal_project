6780a6bac409ae3179bbdce6ac4750d1b4333acd
Ngaicode <Mjombakevin376@gmail.com>
2022-01-20 23:45:33 +0300

    a switch statement is better suited for this particular use case

--- Developer skills/script.js
+++ Developer skills/script.js
@@ -11,51 +11,51 @@ for (let i = 0; i < dayarray.length; i++) {
   let day = dayarray[i];
 
   // switch statement within a for loop
-  //   switch (day) {
-  //     case "monday": // day === "monday"
-  //       console.log("plan you weekly scedule");
-  //       console.log("go to coding meetup");
-  //       break;
+  switch (day) {
+    case "monday": // day === "monday"
+      console.log("plan you weekly scedule");
+      console.log("go to coding meetup");
+      break;
 
-  //     case "tuesday": // case checks for strict equality
-  //       console.log("prepare theory videos");
-  //       break;
-  //     case "wednesday":
-  //       console.log("work out");
-  //       break;
-  //     case "thursday":
-  //       console.log("revise what you studied throughout the week");
-  //       break;
-  //     case "friday":
-  //       console.log("take the day off");
-  //       break;
+    case "tuesday": // case checks for strict equality
+      console.log("prepare theory videos");
+      break;
+    case "wednesday":
+      console.log("work out");
+      break;
+    case "thursday":
+      console.log("revise what you studied throughout the week");
+      break;
+    case "friday":
+      console.log("take the day off");
+      break;
 
-  //     case 203:
-  //       console.log("this is  a number");
-  //       break;
-  //     case "saturday":
-  //     case "sunday":
-  //       console.log("take a chill pill");
-  //     //   this is basically like the else statement,if no condition was met,this will then be run
-  //     default:
-  //       console.log("this is not in the array");
-  //   }
+    case 203:
+      console.log("this is  a number");
+      break;
+    case "saturday":
+    case "sunday":
+      console.log("take a chill pill");
+    //   this is basically like the else statement,if no condition was met,this will then be run
+    default:
+      console.log("this is not in the array");
+  }
 
   // solving the same problem using an if else statement
-  if (day === "monday") {
-    console.log("plan you weekly scedule");
-    console.log("go to coding meetup");
-  } else if (day === "tuesday") {
-    console.log("prepare teory videos");
-  } else if (day === "wednesday") {
-    console.log("work out");
-  } else if (day === "thursday") {
-    console.log("revise what you studied throughout the week");
-  } else if (day === "friday") {
-    console.log("take the day off");
-  } else if (day === "saturday" || day === "sunday") {
-    console.log("take a chill pill");
-  } else {
-    console.log("this is not in the array");
-  }
+  //   if (day === "monday") {
+  //     console.log("plan you weekly scedule");
+  //     console.log("go to coding meetup");
+  //   } else if (day === "tuesday") {
+  //     console.log("prepare teory videos");
+  //   } else if (day === "wednesday") {
+  //     console.log("work out");
+  //   } else if (day === "thursday") {
+  //     console.log("revise what you studied throughout the week");
+  //   } else if (day === "friday") {
+  //     console.log("take the day off");
+  //   } else if (day === "saturday" || day === "sunday") {
+  //     console.log("take a chill pill");
+  //   } else {
+  //     console.log("this is not in the array");
+  //   }
 }
