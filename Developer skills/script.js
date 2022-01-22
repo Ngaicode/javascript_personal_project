797ec8b88a5389e0dc01e86bd80d2ad2823e9be2
Ngaicode <Mjombakevin376@gmail.com>
2022-01-22 15:13:39 +0300

    having fun with switch statements

--- Developer skills/script.js
+++ Developer skills/script.js
@@ -1,3 +1,26 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 console.log("meooow");
+
+// prompts the user for input and then stores it into the meow variable
+let meow = prompt("whom do you love ?");
+
+// if (meow === "peace") {
+//   console.log("I love you silly goose ");
+// } else {
+//   console.log("You are not her");
+// }
+
+switch (meow) {
+  case "peace":
+    alert("I adore you");
+    break;
+  case "muthoni":
+    alert("I feel so good when I spend time with you");
+    break;
+  case "amani":
+    alert("Thank you for being in my life");
+    break;
+  default:
+    alert("Sorry ,you are not the one");
+}
