858a7232caf8365f65fba922bd62d1664887cb7c
Ngaicode <Mjombakevin376@gmail.com>
2022-01-27 19:20:22 +0300

    learning how to use google chrome's inbuilt debugger

--- Developer skills/debugging.js
+++ Developer skills/debugging.js
@@ -8,7 +8,8 @@ const measureKelvin = () => {
     type: "temp",
     unit: "celsius",
     // FIXING THE BUG
-    value: Number(prompt("Degree celsius")),
+    // value: Number(prompt("Degree celsius")),
+    value: 10,
   };
   console.log(measurement);
   //   console.table() gives us the properties and values of objects in table format
@@ -20,3 +21,33 @@ const measureKelvin = () => {
 };
 // IDENTIFYING THE BUG
 console.log(measureKelvin());
+
+// debugging using the debugger in google chrome
+const calcTempAmplitudeBug = function (_t1, _t2) {
+  // the concat method joins the two arrays into one
+  const temps = _t1.concat(_t2);
+  console.log(temps);
+
+  // this statement assumes that the first element of the selected aray is the maximum temprature
+  let max = 0;
+  // this statement assumes that the first element in the array is the minimum temperature
+  let min = 0;
+  // for loop going through the elements of the selected array to check for the maximum and minimum temperature
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+    // this statement skips over elements in the array that are not numbers
+    if (typeof currentTemp !== "number") continue;
+    // if the current temp is greater than the max,then that should become the max
+    if (currentTemp > max) max = currentTemp;
+    // if the current temp is less than the min ,then that should become the min
+    if (currentTemp < min) min = currentTemp;
+  }
+  console.log(max);
+  console.log(min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug(
+  [11, 2, "meow", 3],
+  [10, 1, 12, 15, 9, 17]
+);
+console.log(amplitudeBug);
