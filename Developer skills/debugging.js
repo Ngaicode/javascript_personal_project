23903b5a83615ce160587cf6ce30c8b0e1c2b01b
Ngaicode <Mjombakevin376@gmail.com>
2022-01-29 10:50:13 +0300

    added a liitle comment

--- Developer skills/debugging.js
+++ Developer skills/debugging.js
@@ -72,6 +72,7 @@ const data2 = [12, 5, -5, 0, 4];
 const printForecast = (arr) => {
   let string = "";
   for (let i = 0; i < arr.length; i++) {
+    // similar to  string +=  `...${arr[i]}°C in ${i + 1} days `;
     string = string + `...${arr[i]}°C in ${i + 1} days `;
   }
   console.log(string);
